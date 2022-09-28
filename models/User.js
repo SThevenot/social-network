d799ee78204fd23c1bb712f55672e97c4e6fed41
SThevenot <sktmh007@gmail.com>
2022-09-28 12:00:09 -0500

    created User model and server.js

---
+++ models/User.js
@@ -0,0 +1,44 @@
+/** @format */
+
+const { Schema, model } = require("mongoose");
+
+const userSchema = new Schema(
+  {
+    username: { type: String, required: true, unique: true, trim: true },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      validate: {
+        validator: validator.isEmail,
+        message: `{VALUE} is not a valid email`,
+      },
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+userSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
+});
+
+const User = model("user", userSchema);
+
+module.exports = User;
