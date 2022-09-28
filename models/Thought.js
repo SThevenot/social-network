1a5acc4da048fbbfb413a9d7af5abbf689335e12
SThevenot <sktmh007@gmail.com>
2022-09-28 12:53:03 -0500

    added models for reactions and thoughts

---
+++ models/Thought.js
@@ -0,0 +1,27 @@
+/** @format */
+
+const { Schema, model } = require("mongoose");
+
+const thoughtSchema = new Schema(
+  {
+    thoughtText: { type: String, required: true, maxLength: 280, minLength: 1 },
+    createdAt: { type: Date, default: Date.now },
+    username: { type: String, required: true },
+    reactions: [
+      {
+        type: Schema.Types.DocumentArray,
+        ref: "reactionSchema",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
+thoughtSchema.virtual("reactionCount").get(function () {
+  return this.reactions.length;
+});
+
+const Thought = model("thought", thoughtSchema);
