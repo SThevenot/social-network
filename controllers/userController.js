89918bfee9c755ed11ffd07df01ab9aa45fc2522
SThevenot <sktmh007@gmail.com>
2022-09-29 11:45:30 -0500

    worked on utils and seeding data

---
+++ controllers/userController.js
@@ -0,0 +1,21 @@
+/** @format */
+
+const { User, Application } = require("../models");
+
+module.exports = {
+  getUsers(req, res) {
+    User.find()
+      .then((users) => res.json(users))
+      .catch((err) => res.status(500).json(err));
+  },
+  getSingleUser(req, res) {
+    User.findOne({ _id: req.params.userId })
+      .select("-__v")
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: "User not found" })
+          : res.json(user)
+      )
+      .catch((err) => res.json(500).json(err));
+  },
+};
---
+++ routes/api/user-routes.js
@@ -0,0 +1,20 @@
+/** @format */
+
+const router = require("express").Router();
+const {
+  getUsers,
+  getSingleUser,
+  createUser,
+  updateSingleUser,
+  deleteSingleUser,
+} = require("../../controllers/userController");
+
+router.route("/").get(getUsers).post(createUser);
+
+router
+  .route("/:userId")
+  .get(getSingleUser)
+  .put(updateSingleUser)
+  .delete(deleteSingleUser);
+
+module.exports = router;
